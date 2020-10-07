1ebff819737568e2cdc143b1bf1cbe12bbb0bdcd
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-10-07 17:05:33 +0900

    The Thrid test 1

--- BowlingGame/src/Game.java
+++ BowlingGame/src/Game.java
@@ -2,13 +2,20 @@
 public class Game {
 
 	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+	
 	public void roll(int pins) {
 		// TODO Auto-generated method stub
 		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
 		// TODO Auto-generated method stub
+		int score = 0;
+		for (int i=0; i<rolls.length; i++)
+			score += rolls[i];
 		return score;
 	}
 
