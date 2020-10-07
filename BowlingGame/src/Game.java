94facf64eaf60b3e0c1c6678893513760591e147
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-10-07 16:10:20 +0900

    The first test

---
+++ BowlingGame/src/Game.java
@@ -0,0 +1,14 @@
+
+public class Game {
+
+	public void roll(int pins) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object score() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
