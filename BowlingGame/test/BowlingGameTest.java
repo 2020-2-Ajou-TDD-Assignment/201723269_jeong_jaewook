0d4eca5128674712d8042d95e80b4988151a76a0
JaeWook Jeong <br12345678@ajou.ac.kr>
2020-10-07 16:57:53 +0900

    The Second test 2

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -11,18 +11,23 @@ public class BowlingGameTest {
 	public void setUp() {
 		g = new Game();
 	}
-	@Test
-	public void testGutterGame() {
-		for(int i=0; i<20; i++) {
-			g.roll(0);
+	
+	private void rollMany(int n, int pins) {
+		for(int i=0; i<n; i++) {
+			g.roll(pins);
 		}
-		assertEquals(0, g.score());
 	}
 	
 	@Test
+	public void testGutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+	
+	
+	@Test
 	public void testAllOnes() {
-		for(int i=0; i<20; i++)
-			g.roll(1);
+		rollMany(20,1);
 		assertEquals(20, g.score());
 	}
 }
